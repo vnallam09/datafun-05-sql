8cd5acd02f178a289e20acf3a8c7ee6d4f5ac051
Teja <nallamothuvenkat09@gmail.com>
2025-09-24 16:58:11 -0400

    Added sql

---
+++ create_sql/firstsample.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE authors (
+--     author_id TEXT PRIMARY KEY,
+--     first TEXT NOT NULL,
+--     last TEXT NOT NULL
+-- );
+
+-- INSERT INTO authors(author_id, first, last)
+-- VALUES ("16f3e0a1-25cb-4ed6-a50d-509f63e367f7", "J.W.R." , "Tojur");
+
+SELECT * FROM authors;
